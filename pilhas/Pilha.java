7602d5eec7708aa9daebd15776e8279a6532968f
Lucaslima20 <83251012+Lucaslima20@users.noreply.github.com>
2022-06-14 17:53:16 -0300

    Add files via upload

---
+++ pilhas/Pilha.java
@@ -0,0 +1,15 @@
+
+public class Pilha<T> {
+
+    public void empilha(int num) {
+    }
+
+    public Integer desempilha() {
+        return null;
+    }
+
+    public boolean estaVazia() {
+        return false;
+    }
+
+}
---
+++ pilhas/questão3.java
@@ -0,0 +1,36 @@
+public class questão3 {
+    public static void main(String[] args) {
+		
+		imprimeResultado("ADA");
+		
+		imprimeResultado("ABCD");
+		
+		imprimeResultado("ABCCBA");
+		
+		imprimeResultado("Lucas");
+	}
+	
+	public static void imprimeResultado(String palavra){
+		System.out.println(palavra + " é palindromo? " + testaPalindromo(palavra));
+	}
+
+	public static boolean testaPalindromo(String palavra){
+		
+		Pilha<Character> pilha = new Pilha<Character>(); 
+		
+		for (int i=0; i<palavra.length(); i++){
+			pilha.empilha(palavra.charAt(i));
+		}
+		
+		String palavraInversa = "";
+		while (!pilha.estaVazia()){
+			palavraInversa += pilha.desempilha();
+		}
+		
+		if (palavraInversa.equalsIgnoreCase(palavra)){
+			return true;
+		}
+		
+		return false;
+	}
+}
